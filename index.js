c3beab214025e2422109113670b9519ff0fd1904
shreyasujithk <157006953+shreyasujithk@users.noreply.github.com>
2024-02-15 10:26:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,18 +10,26 @@ app.get("/", function(req, res){
     res.render("page");
 });
 
-// use our puclic/chat.js file as listener
+// use our public/chat.js file as listener
 app.use(express.static(__dirname + '/public'));
-// Set port
-var midPort = app.listen(port, function () {
-    console.log('Node.js listening on port ' + port);
-})
 
-var io = require('socket.io').listen(midPort);
-// set up socket connection
-io.sockets.on('connection', function (socket) {
+// Set port
+var server = app.listen(port, function () {
+    console.log('Node.js listening on port ' + port);
+});
+
+var io = require('socket.io')(server);
+
+// Set up socket connection
+io.on('connection', function (socket) {
+    // Emit a welcome message to the newly connected client
     socket.emit('message', { message: 'Welcome to the Real Time Web Chat' });
+    
+    // Broadcast message to all clients when a new client connects
+    socket.broadcast.emit('message', { message: 'A new user has joined the chat' });
+    
+    // Listen for messages from clients and broadcast them to all clients
     socket.on('send', function (data) {
-        io.sockets.emit('message', data);
+        io.emit('message', data);
     });
 });
